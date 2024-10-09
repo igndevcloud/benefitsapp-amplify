0fcc0fe6d85b4f1e3ee4b9a07437d1a0aa6a16c1
Ignatius Narchetty <inarchet@amazon.com>
2024-10-09 14:48:45 -0500

    adds amazon login

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -3,16 +3,30 @@ import { defineAuth, secret } from '@aws-amplify/backend';
 export const auth = defineAuth({
   loginWith: {
     externalProviders: {
-      google: {
-        clientId: secret('googleClientId'),
-        clientSecret: secret('googleClientSecret')
-      },
-
+      // google: {
+      //   clientId: secret('470953570605-und1qach8tq1m5g2393doi9okvq4l1ue.apps.googleusercontent.com'),
+      //   clientSecret: secret('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')
+      // },
+      // signInWithApple: {
+      //   clientId: secret('SIWA_CLIENT_ID'),
+      //   keyId: secret('SIWA_KEY_ID'),
+      //   privateKey: secret('SIWA_PRIVATE_KEY'),
+      //   teamId: secret('SIWA_TEAM_ID')
+      // },
+       loginWithAmazon: {
+         clientId: secret('amazonClientId'),
+         clientSecret: secret('amazonClientSecret'),
+         scopes: ['email']
+       },
+      // facebook: {
+      //   clientId: secret('FACEBOOK_CLIENT_ID'),
+      //   clientSecret: secret('FACEBOOK_CLIENT_SECRET')
+      // },
       callbackUrls: [
         'http://localhost:3000/profile',
-        'https://mywebsite.com/profile'
+        'https://main.d3dicbp24kdxbr.amplifyapp.com/profile'
       ],
-      logoutUrls: ['http://localhost:3000/', 'https://mywebsite.com'],
+      logoutUrls: ['http://localhost:3000/', 'https://main.d3dicbp24kdxbr.amplifyapp.com/'],
     }
   }
 });
