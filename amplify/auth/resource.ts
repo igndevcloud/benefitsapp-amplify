0969d212e4635b7096d208d4b81f38b630de1a2d
Ignatius Narchetty <inarchet@amazon.com>
2024-10-09 14:14:07 -0500

    removes unwanted login

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -7,20 +7,7 @@ export const auth = defineAuth({
         clientId: secret('googleClientId'),
         clientSecret: secret('googleClientSecret')
       },
-      // signInWithApple: {
-      //   clientId: secret('SIWA_CLIENT_ID'),
-      //   keyId: secret('SIWA_KEY_ID'),
-      //   privateKey: secret('SIWA_PRIVATE_KEY'),
-      //   teamId: secret('SIWA_TEAM_ID')
-      // },
-      // loginWithAmazon: {
-      //   clientId: secret('LOGINWITHAMAZON_CLIENT_ID'),
-      //   clientSecret: secret('LOGINWITHAMAZON_CLIENT_SECRET')
-      // },
-      // facebook: {
-      //   clientId: secret('FACEBOOK_CLIENT_ID'),
-      //   clientSecret: secret('FACEBOOK_CLIENT_SECRET')
-      // },
+
       callbackUrls: [
         'http://localhost:3000/profile',
         'https://mywebsite.com/profile'
@@ -29,6 +16,3 @@ export const auth = defineAuth({
     }
   }
 });
-
-//Google client id - 470953570605-und1qach8tq1m5g2393doi9okvq4l1ue.apps.googleusercontent.com
-//Google client secret - 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
